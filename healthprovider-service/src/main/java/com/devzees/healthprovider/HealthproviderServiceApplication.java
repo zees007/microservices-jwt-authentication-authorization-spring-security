2dafc3445c2b494d6d57249319886dd0543bd6be
zees007 <zees007@rediffmail.com>
2024-09-02 22:09:02 +0530

    first commit

---
+++ healthprovider-service/src/main/java/com/devzees/healthprovider/HealthproviderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devzees.healthprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthproviderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthproviderServiceApplication.class, args);
+	}
+
+}
---
+++ healthprovider-service/src/test/java/com/devzees/healthprovider/HealthproviderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devzees.healthprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthproviderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
