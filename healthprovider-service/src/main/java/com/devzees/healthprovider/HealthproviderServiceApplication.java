9d867f18a007bf00e725407dfa595d19ef0cfe53
zees007 <zees007@rediffmail.com>
2024-10-26 17:04:35 +0530

    service-registry implemented and registered all services

--- healthprovider-service/src/main/java/com/devzees/healthprovider/HealthproviderServiceApplication.java
+++ healthprovider-service/src/main/java/com/devzees/healthprovider/HealthproviderServiceApplication.java
@@ -2,8 +2,10 @@ package com.devzees.healthprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HealthproviderServiceApplication {
 
 	public static void main(String[] args) {
