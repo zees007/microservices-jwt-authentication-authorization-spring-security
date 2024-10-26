c50adb447041f2cc2860cc9d070e78feade8b93b
zees007 <zees007@rediffmail.com>
2024-10-26 21:03:34 +0530

    Annotated the restClient Config with @LoadBalanced

--- appointment-service/src/main/java/com/devzees/appointment/configs/RestClientConfig.java
+++ appointment-service/src/main/java/com/devzees/appointment/configs/RestClientConfig.java
@@ -1,6 +1,7 @@
 package com.devzees.appointment.configs;
 
 import com.devzees.appointment.clients.HealthProviderClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -17,6 +18,7 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Configuration
 public class RestClientConfig {
 
+    @LoadBalanced
     @Bean
     RestClient.Builder restClientBuilder() {
         return RestClient.builder();
